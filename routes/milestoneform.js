42d1290f5693cb8d8c7b292b1fbee4180d14ba5e
椎木 陽介 <shiiki.yosuke@fixer.co.jp>
2018-04-05 19:15:49 +0900

    add validation in milestorneform

--- routes/milestoneform.js
+++ routes/milestoneform.js
@@ -1,15 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+      host    : 'webapp-shiiki-mysqldbserver2.mysql.database.azure.com',
+      user    : 'mysqldbuser@webapp-shiiki-mysqldbserver2',
+      password: 'elec2EYoh',
+      database: 'mysqldatabase57520',
+      charset : 'utf8'
+    }
+});
+
+var bookshelf = require('bookshelf')(knex);
+var Milestoneform = bookshelf.Model.extend({
+    tableName: 'milestoneform'
+});
 
 router.get('/', (req, res, next) => {
     res.render('milestone/milestoneform');
 });
 
 
-router.post('/post', (req, res, next) => {
-    var response = res;
-    new Sample(req.body).save().then((model) => {
-        response.redirect('/');
+router.post('/confirm', (req, res, next) => {
+    var answers = req.body;
+    console.log(JSON.stringify(answers));
+    var i = 0;
+    res.render('milestone/milestoneconfirm.ejs', {
+        answers : answers,
+        answer1 : answers.milestone[i],
+        answer2 : answers.eshours[i],
     });
 });
 
